aef93df5203921faba3ad3d8855c86c6ba8d2109
Akkkkky <coding_addiction@yahoo.co.jp>
2019-08-24 08:28:13 +0900

    Add files via upload

--- config/application.rb
+++ config/application.rb
@@ -6,12 +6,10 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module TeamLesson02
+module MusicConcierge002
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
-    # config.paths.add 'lib', eager_load: true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_13_005040) do
+ActiveRecord::Schema.define(version: 2019_08_23_221939) do
 
-  create_table "spotifies", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "name"
     t.text "mail_address"
     t.datetime "created_at", null: false
